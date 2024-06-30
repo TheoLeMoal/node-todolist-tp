0f4415ccbd0f168f99eb9806e3d3554e3de6303e
Theo <contact@theo-le-moal.fr>
2024-06-30 19:33:23 +0200

    petite modif sur les routes

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,22 @@ const {
     newTaskForm
 } = require('../controllers/index');
 
+/**
+ * Je n'arrive pas a utiliser les routes delete et put pour les formulaire mais sinon avec postman ça fonctionne !
+ */
+
 // Routes pour les tâches
 router.get('', home);
 router.get('/tasks', getTasks);
 router.get('/tasks/new', newTaskForm);
-router.post('/tasks/create', createTask);
-router.get('/tasks/:id', getTaskById);
 router.post('/tasks', createTask);
-router.delete('/tasks/:id', deleteTask);
-router.put('/tasks/:id', updateTask);
-
+router.get('/tasks/:id', getTaskById);
 router.post('/tasks/:id/status', updateTaskStatus);
 router.get('/tasks/:id/edit', editTaskForm);
 router.post('/tasks/:id/update', updateTask);
 router.post('/tasks/:id/delete', deleteTask);
 
+//router.delete('/tasks/:id', deleteTask);
+//router.put('/tasks/:id', updateTask);
 
 module.exports = router;
